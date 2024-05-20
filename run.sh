2748da25f5005e60957a91b2662c57e78acfe2bf
ALFIAN-code <allvvnt@gmail.com>
2024-05-21 02:10:26 +0700

    2593

--- run.sh
+++ run.sh
@@ -1,14 +1,15 @@
-#!/usr/bin/bash
+#!/bin/bash
 
-while [ true ]; do
-    echo 'this is commit ' >> create.txt
-    
-    git add .
-    
-    RANDOM=$$$(date +%s)
-    git commit -m "$RANDOM"
-    
-    
-    git push -u origin2 main
-    sleep 43200s
-done
+# sleep 30s
+cd ~/.config/hypr/reboisasi/
+echo 'this is commit ' >> create.txt
+
+git add .
+
+RANDOM=$$$(date +%s)
+git commit -m "$RANDOM"
+
+
+git push -u origin2 main
+
+dunstify "t2" -a "Commited" 
