d97de386386f32cf0395cd40447d3756e10f92de
ALFIAN-code <allvvnt@gmail.com>
2024-11-01 12:18:06 +0700

    update script

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# sleep 30s
 cd ~/.config/hypr/reboisasi/
+
 echo 'this is commit ' >> create.txt
 
 git add .
@@ -9,6 +9,11 @@ git add .
 RANDOM=$$$(date +%s)
 git commit -m "$RANDOM"
 
+# Loop until there's an internet connection
+while ! ping -c 1 -W 1 google.com &> /dev/null; do
+    sleep 5  
+done
+
 git push -u origin2 main
 
-dunstify "Commited" 
+dunstify "Commited"
